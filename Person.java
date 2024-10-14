9a9f8646abf2f7cbe192b6b8bf7aba9868336a9a
Colpax <Etonespravedlivo@gmail.com>
2024-10-14 01:05:41 +0000

    Person class finished(no comments)

--- Person.java
+++ Person.java
@@ -1,30 +1,43 @@
-public class Person
+/**
+ * Class for creating person object
+ * 
+ * @author Artem Iakovlev
+ * @version 1
+ */
+
+public class Person implements Comparable
 {
 	/***** TODO: (Part 2) create helper inner class for Identity*****/
 
 	// CONSTANT VARIABLES
 	public static final String DEFAULT_NAME = "Jamie Doe";
-	public static final String DEFAULT_STORY =  "Unknown";
+	public static final String DEFAULT_PRONOUNS = "he/him/his";
+	public static final String DEFAULT_BACKGROUND =  "Unknown";
 	public static final int DEFAULT_PRIVILEGE = 100;
 
 	// INSTANCE VARIABLES
-	private String name, story;
+	private String name;
+	private Identity identity;
 	private int privilege;
 
 	// CONSTRUCTORS	
-	public Person(String name, String story, int privilege) {
-		this.setAll(name, story, privilege);
+	public Person(String name, String pronouns, String background, int privilege) {
+		this.setName(name);
+		this.setPrivilege(privilege);
+		identity = new Identity(pronouns, background);
 	}
 		
 	public Person() {
-		this(DEFAULT_NAME, DEFAULT_STORY, DEFAULT_PRIVILEGE);
+		this(DEFAULT_NAME, DEFAULT_PRONOUNS, DEFAULT_BACKGROUND, DEFAULT_PRIVILEGE);
 	}
 	
 	public Person(Person original) {
 		if(original == null) {
 			throw new IllegalArgumentException("Cannot copy null obect in Person copy constructor");
 		} else {
-			this.setAll(original.name, original.story, original.privilege);
+			this.setName(original.name);
+			this.setPrivilege(original.privilege);
+			identity = new Identity(original.getPronouns(), original.getBackground());
 		}
 	}
 
@@ -33,18 +46,23 @@ public class Person
 		this.name = name;
 	}
 
-	public void setStory(String story) {
-		this.story = story;
+	public void setPronouns(String pronouns) {
+		identity.pronouns = pronouns;
+	}
+
+	public void setBackground(String background) {
+		identity.background = background;
 	}
 
 	public void setPrivilege(int privilege) {
 		this.privilege = privilege;
 	}
 
-	public void setAll(String name, String story, int privilege) {
+	public void setAll(String name, String pronoun, String background, int privilege) {
 		this.setPrivilege(privilege);
 		this.setName(name);
-		this.setStory(story);
+		this.setPronouns(pronoun);
+		this.setBackground(background);
 	}
 
 	// ACCESSORS / GETTERS
@@ -52,8 +70,12 @@ public class Person
 		return this.name;
 	}
 		
-	public String getStory() {
-		return this.story;
+	public String getPronouns() {
+		return identity.pronouns;
+	}
+
+	public String getBackground() {
+		return identity.background;
 	}
 
 	public int getPrivilege() {
@@ -64,7 +86,7 @@ public class Person
 	@Override
 	public String toString()
 	{
-		return "My name is "+ this.name + " and "+ this.story + "\n"
+		return "My name is "+ this.name + ". "+ identity.toString() + "\n"
 				+ "According to this calculator I ended up with "+ this.privilege + " estimated privilege points";
 	}
 	
@@ -76,10 +98,71 @@ public class Person
 		}
 		
 		Person otherPerson = (Person) other;
-		return this.name.equals(otherPerson.name) && this.story.equals(otherPerson.story) &&
-			this.privilege == otherPerson.privilege;	
+		return this.name.equals(otherPerson.name) &&
+				identity.equals(otherPerson.identity) &&
+				this.privilege == otherPerson.privilege;
 	}
 
 	// INTERFACE METHODS
 	/***** TODO: (Part 1) override compareTo method to implement Comparable interface*****/
+
+	@Override
+	public int compareTo(Object o) {
+		if (o == null) {
+			return -1;
+		} else if (!(o instanceof Person)) {
+			return -1;
+		} else {
+			Person p = (Person) o;
+			return privilege - p.privilege;
+		}
+	}
+
+	/**
+ 	* Class for creating Identity for a person
+ 	* 
+ 	* @author Artem Iakovlev
+ 	* @version 1
+ 	*/
+	public class Identity {
+		// INSTANCE VARIABLES
+		private String pronouns;
+		private String background;
+
+		// CONSTRUCTORS	
+		/**
+		* Full construcnor for Identity class
+		* 
+		* @param String pronouns
+		* @param String background
+		*/
+		public Identity(String pronouns, String background) {
+			this.pronouns = pronouns;
+			this.background = background;
+		}
+
+		/**
+		* Default construcnor for Identity class, uses default variables
+		*/
+		public Identity() {
+			this(DEFAULT_PRONOUNS, DEFAULT_BACKGROUND);
+		}
+
+		@Override
+		public String toString() {
+			return "My pronouns are: " + pronouns + ". My story is: " + background;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (o == null) {
+				return false;
+			} else if (!(o instanceof Identity)) {
+				return false;
+			} else {
+				Identity id = (Identity) o;
+				return background.equals(id.background) && pronouns.equals(id.pronouns);
+			}
+		}
+	}
 }
